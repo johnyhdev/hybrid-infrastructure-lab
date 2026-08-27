c4143aaa685f73992192ba121040746b813d2faf
Johnydev <johnyhdev@gmail.com>
2026-08-27 14:16:06 +0700

    ci: add terraform plan workflow and update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,8 @@
 terraform {
   backend "azurerm" {
+    use_oidc         = true
+    use_azuread_auth = true
+
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "sthybridlab260826"
     container_name       = "tfstate"
