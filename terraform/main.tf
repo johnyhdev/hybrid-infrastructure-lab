2232b1f90735e51c123ad6ce5731208c4aba447d
Johnydev <johnyhdev@gmail.com>
2026-08-29 11:03:51 +0700

    Update with security risk

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,18 @@ resource "azurerm_network_security_group" "vm" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
+  security_rule {
+    name                       = "allow-ssh-internet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 resource "azurerm_subnet_network_security_group_association" "management" {
