297f951260ab2d5b42d7f3f71bbfa10192973622
Johnydev <johnyhdev@gmail.com>
2026-08-29 10:27:58 +0700

    Commit legit security

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,7 @@ resource "azurerm_network_security_group" "vm" {
   name                = "nsg-vm"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+
 }
 
 resource "azurerm_subnet_network_security_group_association" "management" {
