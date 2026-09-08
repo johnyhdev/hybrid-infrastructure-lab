00b3ad0d4545a6523192f90d428e800f19875573
Johnydev <johnyhdev@gmail.com>
2026-09-08 15:52:36 +0700

    Update terraform apply v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Phase 3 OIDC CI test
 # Phase 4 terraform-apply.yaml test new
-# phase 5 terraform apply check controlled apply v2
+# phase 5 terraform apply check controlled apply v3
 
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
